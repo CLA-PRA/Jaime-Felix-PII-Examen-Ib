b80d1ec378add6a6ce92bcc0ae61792ddeca4576
Jaime-Felix <jaime.fm@culiacan.tecnm.mx>
2024-02-27 21:19:12 -0700

    Agregar archivos de proyecto.

---
+++ Examen-Ib/Examen_Ib.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Examen_Ib
+{
+    internal class Examen_Ib
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
